c0a5a26742b79475e6bf8485c32f96414f74ea4d
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2024-12-02 09:36:27 +0530

    create user delete function

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,6 +65,17 @@ export function loginUser(req,res){
         }
       }
     }
-  )  
+  ) 
+}  
+
+export function deleteUser(req,res){  
+  User.deleteOne({email : req.body.email}).then(()=>{
+    res.json({
+      message : "student deleted"
+    })
+  }
+    
+  )
   
-}
+};
+
