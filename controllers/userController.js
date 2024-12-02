3184dfb1b5664851e294164534fe110bece42d03
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2024-12-02 10:53:30 +0530

    removing the delete user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,14 +68,5 @@ export function loginUser(req,res){
   ) 
 }  
 
-export function deleteUser(req,res){  
-  User.deleteOne({email : req.body.email}).then(()=>{
-    res.json({
-      message : "user successfully deleted"
-    })
-  }
-    
-  )
-  
-};
+
 
