f7417bd7123dff3ad1ef29c08048a86439550d76
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-02-16 00:47:28 +0530

    search product part added to product router

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,11 +4,13 @@ import { getProducts } from '../controllers/productController.js';
 import { deleteProduct } from '../controllers/productController.js';
 import { updateProduct } from '../controllers/productController.js';
 import { getProduct } from '../controllers/productController.js';
+import { searchProducts } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct);
 productRouter.get("/",getProducts);
+productRouter.get("/search/:query",searchProducts);
 productRouter.get("/:productId",getProduct);
 productRouter.delete("/:productId",deleteProduct);
 productRouter.put("/:productId",updateProduct);
