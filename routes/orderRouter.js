6d1719e6b0301854539f202939098f2160be9cf4
Matheesha Kalatuwawa <pamodmatheesha2020@gmail.com>
2025-02-20 20:36:46 +0530

    changed orderRouter

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,8 +1,5 @@
 import express from 'express';
-import { createOrder } from '../controllers/orderController.js';
-import { getOrders } from '../controllers/orderController.js';
-import { getQuote } from '../controllers/orderController.js';
-
+import { createOrder, getOrders, getQuote } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
